4e8a1523c3b5b24ab5e76cce7198f8cb6ac33004
Cris Silva Jr <ishiikurisu@yahoo.com>
2017-02-27 21:49:07 -0300

    Generating output file name

--- minimize.lua
+++ minimize.lua
@@ -1,8 +1,21 @@
 -- MAIN STRUCTURE
 local minimizer = { }
 
+minimizer.changeExtension = function(input, newExt)
+  local index = #input
+
+  while input:sub(index, index) ~= '.' do
+      index = index - 1
+  end
+
+  local root = input:sub(1, index-1)
+  return root .. newExt
+end
+
 minimizer.minimize = function(input)
   print("input: " .. input)
+  local output = minimizer.changeExtension(input, '.min.lua')
+  print("output: " .. output)
 end
 
 -- MAIN FUNCTIONS
