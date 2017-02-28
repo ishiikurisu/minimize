2c0d7c642ecfebffdc1d16cbb5a68f8859f99d21
Cris Silva Jr <ishiikurisu@yahoo.com>
2017-02-27 21:57:32 -0300

    Laying out next steps

--- minimize.lua
+++ minimize.lua
@@ -16,6 +16,9 @@ minimizer.minimize = function(input)
   print("input: " .. input)
   local output = minimizer.changeExtension(input, '.min.lua')
   print("output: " .. output)
+  local references = { }
+  -- TODO Scan main file
+  -- TODO Scan every file in reference until there are no more files
 end
 
 -- MAIN FUNCTIONS
