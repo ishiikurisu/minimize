8e59b6528208ede1e58a6dceb8041e8850d8e407
Cristiano Silva Jr <cristiano.a.junior@ieee.org>
2017-03-01 11:52:38 -0300

    Scanning main file

--- minimizer/minimizer.lua
+++ minimizer/minimizer.lua
@@ -12,12 +12,35 @@ minimizer.changeExtension = function(input, newExt)
   return root .. newExt
 end
 
+minimizer.identifyRequires = function(fileName, references)
+  fp, error = io.open(fileName)
+
+  line = fp:read()
+  while line ~= nil do
+    -- TODO Identify lines with requires
+    local result = string.find(line, '= require')
+    if result ~= nil then
+      local required = line:sub(result+11, #line-1)
+      table.insert(references, required)
+    end
+    line = fp:read()
+  end
+
+  return references
+end
+
 minimizer.minimize = function(input)
   print("input: " .. input)
   local output = minimizer.changeExtension(input, '.min.lua')
   print("output: " .. output)
   local references = { }
   -- TODO Scan main file
+  print(input .. " requires:")
+  references = minimizer.identifyRequires(input, references)
+  for _, v in pairs(references) do
+    print("- " .. v)
+  end
+  print("...")
   -- TODO Scan every file in reference until there are no more files
 end
 
