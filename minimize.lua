e729675766f33e50a7bc3c4a6723fc107e54876b
Cristiano Silva Jr <cristiano.a.junior@ieee.org>
2017-03-01 11:38:26 -0300

    Separating minimizer from main script for better tests

--- minimize.lua
+++ minimize.lua
@@ -1,25 +1,4 @@
--- MAIN STRUCTURE
-local minimizer = { }
-
-minimizer.changeExtension = function(input, newExt)
-  local index = #input
-
-  while input:sub(index, index) ~= '.' do
-      index = index - 1
-  end
-
-  local root = input:sub(1, index-1)
-  return root .. newExt
-end
-
-minimizer.minimize = function(input)
-  print("input: " .. input)
-  local output = minimizer.changeExtension(input, '.min.lua')
-  print("output: " .. output)
-  local references = { }
-  -- TODO Scan main file
-  -- TODO Scan every file in reference until there are no more files
-end
+local minimizer = require 'minimizer/minimizer'
 
 -- MAIN FUNCTIONS
 if #arg > 0 then
