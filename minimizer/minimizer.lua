ca6bbb10ebb13e3cb3b1b61f202b2a5db1fa4603
Cristiano Silva Jr <cristiano.a.junior@ieee.org>
2017-03-01 12:02:07 -0300

    Trying to build a tree of dependencies

--- minimizer/minimizer.lua
+++ minimizer/minimizer.lua
@@ -1,4 +1,3 @@
--- MAIN STRUCTURE
 local minimizer = { }
 
 minimizer.changeExtension = function(input, newExt)
@@ -13,7 +12,7 @@ minimizer.changeExtension = function(input, newExt)
 end
 
 minimizer.identifyRequires = function(fileName, references)
-  fp, error = io.open(fileName)
+  local fp, error = io.open(fileName)
 
   line = fp:read()
   while line ~= nil do
@@ -26,6 +25,7 @@ minimizer.identifyRequires = function(fileName, references)
     line = fp:read()
   end
 
+  fp:close()
   return references
 end
 
@@ -34,14 +34,33 @@ minimizer.minimize = function(input)
   local output = minimizer.changeExtension(input, '.min.lua')
   print("output: " .. output)
   local references = { }
-  -- TODO Scan main file
+
+  -- # Scanning main file
   print(input .. " requires:")
   references = minimizer.identifyRequires(input, references)
   for _, v in pairs(references) do
     print("- " .. v)
   end
   print("...")
+
   -- TODO Scan every file in reference until there are no more files
+  local flag = true
+  local added = { }
+
+  -- The flag will turn into true when there are new added files
+  while flag do
+    local tempRefs = references
+    flag = false
+    for _, reference in pairs(tempRefs) do
+      if added[reference] ~= true then
+        print(reference .. ".lua")
+        added[reference] = true
+        flag = true
+        -- TODO Discover why this file can't be opened
+        references = minimizer.identifyRequires(reference .. ".lua", references)
+      end
+    end
+  end
 end
 
 return minimizer
